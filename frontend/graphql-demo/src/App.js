1434731df8cf52374704e962b5df64254d3dc53a
Christoph Haag <haag@codeunity.de>
2022-01-18 07:32:41 +0100

    use subscription

--- frontend/graphql-demo/src/App.js
+++ frontend/graphql-demo/src/App.js
@@ -1,6 +1,20 @@
-function App() {
+import { gql, useSubscription } from "@apollo/client";
+
+const CURRENT_DATE_TIME_SUBSCRIPTION = gql`
+  subscription {
+    currentDateTime
+  }
+`;
+
+const App = () => {
+
+  const { data, loading } = useSubscription(CURRENT_DATE_TIME_SUBSCRIPTION);
+
   return (
-    <div className="h-screen flex justify-center items-center">Hello world!</div>
+    <div className="h-screen flex justify-center items-center text-xl">
+      {loading && <p>lädt...</p>}
+      {data && <p>{data.currentDateTime}</p>}
+    </div>
   );
 }
 
